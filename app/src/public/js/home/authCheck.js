ca129a081ca1a8a7cce098d2f18ba82bb2389a01
Lrbll <dlxkdxs@naver.com>
2023-10-07 02:04:17 +0900

    코드 수정

--- app/src/public/js/home/authCheck.js
+++ app/src/public/js/home/authCheck.js
@@ -3,7 +3,7 @@
 module.exports = {
   isOwner: function (request, response) {
     if (request.session.is_logined) {
-      console.log(request.session.is_logined);
+      // console.log(request.session.is_logined);
       return true;
     } else {
       return false;
